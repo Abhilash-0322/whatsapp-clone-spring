6b7720782a3d64afd988c42d9b32dae24d8848a1
Abhilash-0322 <mauryaabhi2003@gmail.com>
2025-06-09 02:43:10 +0530

    initial

---
+++ whatsappclone/src/main/java/com/example/whatsappclone/WhatsappcloneApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.whatsappclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsappcloneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsappcloneApiApplication.class, args);
+	}
+
+}
